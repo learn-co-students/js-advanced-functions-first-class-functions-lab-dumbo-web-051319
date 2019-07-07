e6a0dbd97d46089aa6c092133ec2090f09d5dd10
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-07-07 17:48:32 -0400

    half done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arrDrivers) {
+  return [arrDrivers[0], arrDrivers[1]];
+}
+
+const returnLastTwoDrivers =  function(arrDrivers) {
+  let lastTwoAndUndefined = [arrDrivers[arrDrivers.length - 2], arrDrivers[arrDrivers.length - 1]];
+  // let lastTwo = lastTwoAndUndefined.filter((driver) => {driver != undefined});
+  // return lastTwo;
+  return lastTwoAndUndefined;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
